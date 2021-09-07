3aa4f4f2b7a69f067bf07cdb7fcd5dbdd8c261e2
karan106 <karankatyal1998@gmail.com>
2021-09-07 19:02:21 +0530

    routing & header , footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { SidebarComponent } from './shared/login/component/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/login/component/header/header.component';
+import { FooterComponent } from './shared/login/component/footer/footer.component';
+import { DashboardComponent } from './core/component/dashboard/dashboard.component';
+const routes: Routes = [
+  {path : 'sidebar' , component:  SidebarComponent},
+  {path : 'header' , component :HeaderComponent },
+  {path : 'footer' , component : FooterComponent },
+  {path : 'dashboard' , component : DashboardComponent},
+  
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './shared/login/login.component';
+import { SidebarComponent } from './shared/login/component/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/login/component/header/header.component';
+import { FooterComponent } from './shared/login/component/footer/footer.component';
+import { DashboardComponent } from './core/component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
